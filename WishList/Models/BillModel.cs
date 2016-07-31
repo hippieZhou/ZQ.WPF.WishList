9dd450e191e490f950310196b561bac0f3d2d429
hippieZhou <hippiezhou@outlook.com>
2016-07-31 15:01:00 +0800

    添加界面基本完成

---
+++ WishList/Models/BillModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WishList.Models
+{
+    [Flags]
+    public enum BillType
+    {
+        /// <summary>
+        /// 收入
+        /// </summary>
+        Income = 0,
+        /// <summary>
+        /// 支出
+        /// </summary>
+        OutLay = 1
+    };
+    public class BillModel
+    {
+        public BillType Type { get; set; }
+
+        public string Tag { get; private set; }
+
+        public DateTime Time { get; set; }
+
+        public ImageSource Image { get; set; }
+    }
+}
