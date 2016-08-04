655e5d53bfaa95574598fc6f293becf439f1d607
hippieZhou <hippiezhou@outlook.com>
2016-08-04 22:42:11 +0800

    完成报表页面中的日历页面打开和关闭功能

--- WishList/ViewModels/AddScheduleViewModel.cs
+++ WishList/ViewModels/AddScheduleViewModel.cs
@@ -1,8 +1,23 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using System.Windows;
 
 namespace WishList.ViewModels
 {
-    public class AddScheduleViewModel:ViewModelBase
+    public class AddScheduleViewModel : ViewModelBase
     {
+        private RelayCommand _backCmd;
+        public RelayCommand BackCmd
+        {
+            get
+            {
+                return _backCmd ?? (_backCmd = new RelayCommand(
+                    () =>
+                    {
+                        Messenger.Default.Send<Visibility>(Visibility.Collapsed, "closeMinorView");
+                    }));
+            }
+        }
     }
 }
--- WishList/ViewModels/ListViewModel.cs
+++ WishList/ViewModels/ListViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace WishList.ViewModels
 {
@@ -7,5 +8,21 @@ namespace WishList.ViewModels
     /// </summary>
     public class ListViewModel:ViewModelBase
     {
+        private RelayCommand _openScheduleCmd;
+
+        /// <summary>
+        /// Gets the OpenScheduleCmd.
+        /// </summary>
+        public RelayCommand OpenScheduleCmd
+        {
+            get
+            {
+                return _openScheduleCmd ?? (_openScheduleCmd = new RelayCommand(
+                    () =>
+                    {
+                        MINORManager.CurrentMinor.PostMinorViewAndViewModel(MINORManager.AddScheduleVM);
+                    }));
+            }
+        }
     }
 }
